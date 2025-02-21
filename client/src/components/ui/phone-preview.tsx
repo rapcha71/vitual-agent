70dce124a7f9fa13535192277e17ba937b9c670e
rapcha1 <39795977-rapcha1@users.noreply.replit.com>
2025-02-21 17:10:23 +0000

    Agent query: ¿Ha desaparecido la barra de desplazamiento del lado derecho mientras mantenemos la funcionalidad de scroll?
    Fix: Remove unnecessary overflow-hidden styles from PhonePreview component and add image asset.
    
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/c2729e48-2fb9-4518-8d14-3149229b69cf/76c2be6e-e01c-42e6-81df-a0f5b57009e4.jpg

--- client/src/components/ui/phone-preview.tsx
+++ client/src/components/ui/phone-preview.tsx
@@ -8,23 +8,23 @@ export function PhonePreview({ children, className, ...props }: PhonePreviewProp
   return (
     <div className={cn(
       "flex items-center justify-center min-h-screen bg-gray-100",
-      "overflow-hidden", //Added to hide scrollbars on the main container
-      "[-ms-overflow-style:'none']", //Added to hide scrollbars on the main container
-      "[scrollbar-width:none]", //Added to hide scrollbars on the main container
-      "[&::-webkit-scrollbar]:hidden" //Added to hide scrollbars on the main container
-
+      "overflow-hidden", 
+      "[-ms-overflow-style:'none']", 
+      "[scrollbar-width:none]", 
+      "[&::-webkit-scrollbar]:hidden" 
     )}>
       <div className={cn(
-        "w-[340px] aspect-[9/16] bg-white overflow-hidden relative",
+        "w-[340px] aspect-[9/16] bg-white relative",
         "border-[8px] border-black rounded-[30px]",
         "before:content-[''] before:absolute before:top-0 before:left-1/2 before:-translate-x-1/2",
         "before:w-20 before:h-4 before:bg-black before:rounded-b-2xl",
         "after:content-[''] after:absolute after:bottom-2 after:left-1/2 after:-translate-x-1/2",
         "after:w-24 after:h-1 after:bg-black after:rounded-full",
         "shadow-[0_8px_16px_rgba(0,0,0,0.1)]",
+        "overflow-hidden",
         className
       )} {...props}>
-        <div className="h-full overflow-y-auto [&::-webkit-scrollbar]:hidden [-ms-overflow-style:'none'] [scrollbar-width:none]">
+        <div className="h-full overflow-y-auto [-ms-overflow-style:'none'] [scrollbar-width:none] [&::-webkit-scrollbar]:hidden">
           {children}
         </div>
       </div>
