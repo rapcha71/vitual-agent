796d205525b0694f4f75b4061e54ad0e18ebd674
rapcha71 <rapcha@gmail.com>
2025-07-01 18:12:32 -0600

    Update drizzle.config.ts
    cambios según últimos resultados

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,14 +1,20 @@
 import { defineConfig } from "drizzle-kit";
+import * as dotenv from "dotenv";
 
+dotenv.config({ path: ".env" });
+
+// Verifica que la variable de entorno esté presente
 if (!process.env.DATABASE_URL) {
-  throw new Error("DATABASE_URL, ensure the database is provisioned");
+  throw new Error("DATABASE_URL is not set in environment variables");
 }
 
 export default defineConfig({
-  out: "./migrations",
-  schema: "./shared/schema.ts",
-  dialect: "postgresql",
+  schema: "./server/db/schema.ts",
+  out: "./drizzle",
+  dialect: "postgresql", // Especificamos que usamos PostgreSQL
   dbCredentials: {
-    url: process.env.DATABASE_URL,
+    url: process.env.DATABASE_URL, // Le decimos que use nuestra variable de entorno
   },
+  verbose: true,
+  strict: true,
 });
